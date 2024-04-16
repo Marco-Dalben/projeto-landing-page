d327a92986f35e0a01fd2ca4245e1fe8ffb7ada0
Marco-Dalben <marco.a.dalben@gmail.com>
2024-04-15 22:16:18 -0300

    Versão Inicial

---
+++ index.js
@@ -0,0 +1,21 @@
+var SetaEsquerda = window.document.getElementById("seta-esquerda")
+var Leonardo = window.document.getElementById("Leonardo")
+var Samantha = window.document.getElementById("Samantha")
+var Bruna = window.document.getElementById("Bruna")
+var SetaDireita = window.document.getElementById("seta-direita")
+
+function RolarParaDireita() {
+    Leonardo.style = "display:none"
+    Bruna.style = "display:flex"
+    SetaEsquerda.style = "display:none"
+    SetaDireita.style = "display:flex; margin-top:55px"
+}
+
+function RolarParaEsquerda() {
+    Leonardo.style = "display:flex"
+    Bruna.style = "display:none"
+    SetaEsquerda.style = "display:flex; margin-top:55px"
+    SetaDireita.style = "display:none"
+   
+
+}
